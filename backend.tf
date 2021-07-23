207f3eb4aeaff4b15b4d26e35e82039ed2d2cf5a
rtekoh <70532179+rtekoh@users.noreply.github.com>
2021-07-22 22:22:07 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,24 @@
 terraform {
   backend "s3" {
-    bucket  = "boytag89"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    dynamodb_table = "tekohjenkins"
+    bucket = "boytag89"
+    region = "us-east-1"
+    key = "dev/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
   }
 }
 
+resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
+   name = "terraform-lock"
+   hash_key = "LockID"
+   read_capacity = 20
+   write_capacity = 20
+
+   attribute {
+      name = "LockID"
+      type = "S"
+   }
+
+   tags {
+     Name = "Terraform Lock Table"
+   }
+}
