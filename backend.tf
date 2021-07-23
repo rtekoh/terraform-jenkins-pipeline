6a1c3a0a5f42833b57af540b0a8502ff0f6c1217
rtekoh <70532179+rtekoh@users.noreply.github.com>
2021-07-22 22:30:56 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,16 +7,5 @@ terraform {
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
-   name = "terraform-lock"
-   hash_key = "LockID"
-   read_capacity = 20
-   write_capacity = 20
-
-   attribute {
-      name = "LockID"
-      type = "S"
-   }
-}
 
 
